128425a037dedda299f70a253cec74704e62777c
amir ahmadi <amirahmadidev@gmail.com>
2023-02-17 00:14:53 +0330

    feat: added appoinment service and repository

--- src/di-container.ts
+++ src/di-container.ts
@@ -1,5 +1,10 @@
+import AppoinmentRepository from '@Application/appoinments.repository'
+import AppoinmentService from '@Application/appoinments.service'
 import { Container } from 'inversify'
 
 export const container = new Container({
   defaultScope: 'Singleton',
 })
+
+container.bind(AppoinmentService).toSelf()
+container.bind(AppoinmentRepository).toSelf()
